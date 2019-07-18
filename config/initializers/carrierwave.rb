cae62eb18801dfd7df20292708fa99498520962c
sakamakiyuma <yuma78733@yahoo.co.jp>
2019-07-18 19:44:00 +0900

    | ←忘れた

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'sakamaki55'
-  config.asset_host = 'https://s3-ap-northeast-1'.amazonaws.com/sakamaki55'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/sakamaki55'
 end
