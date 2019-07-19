ce1d4ea630886cbde0ee5febf4ca82d7806a6955
sakamakiyuma <yuma78733@yahoo.co.jp>
2019-07-19 11:59:25 +0900

    Capistrano 確認

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,5 @@
-# config valid only for current version of Capistrano
-# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '3.11.0'
+lock '~> 3.11.0'
 
-# Capistranoのログの表示に利用する
 set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
@@ -10,7 +7,6 @@ set :repo_url,  'git@github.com:sakamakiyuma/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
@@ -25,9 +21,14 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
 
 # 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
-
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
